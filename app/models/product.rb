d119419f0948ef08789e90f29ecc24f94b4aa5c4
erbdevs <erb.devs@gmail.com>
2021-12-15 19:10:16 +0100

    [2] Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :code, :name, :price, presence: true
+
+  validates :name, length: { minimum: 3 }
+  validates :code,
+            length: { minimum: 3 },
+            format: { with: /[A-Z]{2}\d{1}/, message: 'Invalid format for code' }
 end
