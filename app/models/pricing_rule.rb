67179d44ed120d314df58857dfb2eb6275367133
erbdevs <erb.devs@gmail.com>
2021-12-22 17:42:19 +0100

    [3] Validations for ranges of products to match the rule

--- app/models/pricing_rule.rb
+++ app/models/pricing_rule.rb
@@ -3,13 +3,29 @@ class PricingRule < ApplicationRecord
 
   validates :name, :enabled, presence: true
 
-  validate :valid_type_of_discount?
+  validate :valid_type_of_discount?, :valid_range_of_products_number?
 
   def valid_type_of_discount?
     if unitary_absolute_price.present? && unitary_factor_price.present?
-      errors.add('Only one can be defined')
+      errors.add('only one can be defined')
     elsif unitary_absolute_price.nil? && unitary_factor_price.nil?
       errors.add('One of them must be defined')
     end
+
+    true
+  end
+
+  def valid_range_of_products_number?
+    if max_products_number.nil? && min_products_number.nil?
+      errors.add(:min_products_number, "can't be nil if max_products_number also is nil")
+      return
+    end
+    return true if max_products_number.nil? || min_products_number.nil?
+
+    if max_products_number < min_products_number
+      errors.add(:max_products_number, 'has to be greater than min')
+    end
+
+    true
   end
 end
--- spec/models/pricing_rule_spec.rb
+++ spec/models/pricing_rule_spec.rb
@@ -13,4 +13,57 @@ RSpec.describe PricingRule do
 
     expect(pricing_rule.valid?).to eq(false)
   end
+
+  context "regarding products_number" do
+    context "when MIN and MAX are equal" do
+      it 'is valid' do
+        pricing_rule.min_products_number = 3
+        pricing_rule.max_products_number = 3
+
+        expect(pricing_rule.valid?).to eq(true)
+      end
+    end
+
+    context "when there is only MAX" do
+      it 'is valid' do
+        pricing_rule.min_products_number = nil
+        pricing_rule.max_products_number = 7
+
+        expect(pricing_rule.valid?).to eq(true)
+      end
+    end
+
+    context "when there is only MIN" do
+      it 'is valid' do
+        pricing_rule.min_products_number = 7
+        pricing_rule.max_products_number = nil
+
+        expect(pricing_rule.valid?).to eq(true)
+      end
+    end
+
+    context "when MAX is less than MIN" do
+      it 'is invalid' do
+        pricing_rule.min_products_number = 3
+        pricing_rule.max_products_number = 2
+
+        expect(pricing_rule.valid?).to eq(false)
+        expect(pricing_rule.errors.messages).to eq(
+          { max_products_number: ["has to be greater than min"] }
+        )
+      end
+    end
+
+    context "when both are nil" do
+      it 'is invalid' do
+        pricing_rule.min_products_number = nil
+        pricing_rule.max_products_number = nil
+
+        expect(pricing_rule.valid?).to eq(false)
+        expect(pricing_rule.errors.messages).to eq(
+          { min_products_number: ["can't be nil if max_products_number also is nil"] }
+        )
+      end
+    end
+  end
 end
